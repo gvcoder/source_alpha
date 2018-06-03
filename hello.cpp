6a7d2313a9aa6ae52a353f8f09ee2c4210664fae
gvcoder <39855519+gvcoder@users.noreply.github.com>
2018-06-03 19:08:11 +0530

    Updated the application

--- hello.cpp
+++ hello.cpp
@@ -3,10 +3,7 @@ using namespace std;
 
 // main() is where program execution begins.
 int main() {
-<<<<<<< HEAD
+
    cout << "Hello World from cppx"; // prints Hello World
-=======
-   cout << "Hello World from cpp"; // prints Hello World from cpp
->>>>>>> 95418f974aa5a976ca0f14051d0186b02b841bcc
    return 0;
 }
